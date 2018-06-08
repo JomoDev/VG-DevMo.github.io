8b58afbbd12b0b498f5ded65a77d9f938a0c537f
Jonas <jonas-gruempel@gmx.de>
2018-06-08 21:22:29 +0200

    Color picker implementation

---
+++ Grunt.js
@@ -0,0 +1,11 @@
+grunt.initConfig({
+  concat: {
+    // concat task configuration goes here.
+  },
+  uglify: {
+    // uglify task configuration goes here.
+  },
+  // Arbitrary non-task-specific properties.
+  my_property: 'whatever',
+  my_src_files: ['foo/*.js', 'bar/*.js'],
+});
