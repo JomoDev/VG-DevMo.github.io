de66358b753d187db6d376471e88c5bfba30e619
Jonas <jonas-gruempel@gmx.de>
2018-07-05 22:42:36 +0200

    Grunt serve added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,8 @@
 module.exports = function(grunt) {
-	rules: [{
-    ip: '127.0.0.1',    // point domain "A" to address "B"
-    hostname: 'qun.qq.com',
-    type: 'set'
-}]
-rules: [{
-    domain: 'baidu.com',    // point domain "A" to domain "B"
-    hostname: 'qun.qq.com',
-    type: 'set'
-}]
-
-grunt.initConfig({
-    localhosts: {
-        set : {
-            options: {
-                rules: [{
-                    ip: '127.0.0.1',
-                    hostname: 'qun.qq.com',
-                    type: 'set'
-                }, {
-                    ip: '127.0.0.1',
-                    hostname: 'find.qq.com',
-                    type: 'set'
-                }, {
-                    domain: 'baidu.com',    // point domain "A" to domain "B"
-                    hostname: 'find.qq.com',
-                    type: 'set'
-                }]
-            }
-        },
-        remove : {
-            options: {
-                rules: [{
-                    ip: '127.0.0.1',
-                    hostname: 'qun.qq.com',
-                    type: 'remove'
-                }]
-            }
-        }
-    },
-});
- 
- 
-grunt.registerTask('serve', function (target) {
-    grunt.task.run([
-        'concurrent:server',
-        "localhosts:set",
-        'connect:livereload',
-        'open:server',
-        'watch'
-    ]);
-});
- 
-grunt.registerTask('build', [
-        'localhosts:remove',    // clear previously set localhosts
-        '.......'    // content removed for brevity
-]);
-} 
+	grunt.loadNpmTasks('grunt-serve');
+	grunt.initConfig({
+		'serve': {
+	'path': 'jondijon13.github.io/index.html'
+}
+	});
+}
