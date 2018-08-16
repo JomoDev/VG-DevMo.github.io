f6a8f7f6727c38c00bccc760291b979663328f84
jonas <test@test.com>
2018-08-17 01:15:39 +0200

    added: editable size | editable color

---
+++ src/Enviroment.js
@@ -0,0 +1,76 @@
+var context = document.getElementById('canvas').getContext("2d");
+var paint = false;
+var clickColor = new Array();
+var clickSize = new Array();
+
+
+context.fillStyle = "#ffffff";
+context.fillRect(0,0, context.width, context.height);
+
+$("#canvas").mousedown(function(e){
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
+
+  paint = true;
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+  redraw();
+});
+
+$("#canvas").mousemove(function(e){
+  if(paint){
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    redraw();
+  }
+});
+
+$("#canvas").mouseup(function(e){
+  paint = false;
+});
+
+$("#canvas").mouseleave(function(e){
+  paint = false;
+});
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+  clickColor.push(document.getElementById("color").value);
+  clickSize.push(document.getElementById("size").value);
+}
+
+function redraw(){
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+  //context.strokeStyle = "#df4b26";
+  context.lineJoin = "round";
+
+  for(var i=0; i < clickX.length; i++) {
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+     }else{
+       context.moveTo(clickX[i]-1, clickY[i]);
+     }
+     context.lineTo(clickX[i], clickY[i]);
+     context.closePath();
+     context.strokeStyle = clickColor[i];
+     context.lineWidth = clickSize[i];
+     context.stroke();
+  }
+}
+
+$("#clear").click(function () {
+  clearCanvas();
+});
+
+function clearCanvas()
+{
+	context.clearRect(0, 0, context.Height, context.Width);
+}
