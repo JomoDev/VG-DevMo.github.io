3d1226b20e99643efc3dacb1807b3958656eb039
jonas <jonas-gruempel@gmx.de>
2018-09-02 21:23:07 +0200

    set test

--- Lobby/src/Lobby.controller.js
+++ Lobby/src/Lobby.controller.js
@@ -46,7 +46,7 @@ window.onload = function() {
   function testRequest () {
     $.ajax({ url: '/server.php',
              data: {action: 'test'},
-             type: 'GET',
+             type: 'SET',
              success: function(output) {
                           alert(output);
                       },
