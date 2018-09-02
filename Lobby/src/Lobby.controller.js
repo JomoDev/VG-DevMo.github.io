a51559d5d9253546cbae0e75dc389a48db1a5a22
jonas <jonas-gruempel@gmx.de>
2018-09-02 23:46:09 +0200

    added cookie question

--- Lobby/src/Lobby.controller.js
+++ Lobby/src/Lobby.controller.js
@@ -1,7 +1,35 @@
 window.onload = function() {
 
+  var loadCookie = false;
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  modal.style.display = "block";
+
+  $('#_yes').click(function() {
+    loadCookie=true;
+    modal.style.display = "none";
+    if(loadCookie) {
+      readCookieData();
+    }
+  });
+
+  $('#_no').click(function() {
+
+  });
+
   testRequest();
-  readCookieData();
+  if(loadCookie) {
+    readCookieData();
+  }
 
   function readCookieData () {
     var cookieVal = ud_read_cookie("UserName");
