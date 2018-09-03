fb3a6331b1c2491b1ef91a68547ce50c4ef6bd86
VG-DevMo <jonas-gruempel@gmx.de>
2018-09-03 09:55:17 +0200

    Update Lobby.controller.js

--- Lobby/src/Lobby.controller.js
+++ Lobby/src/Lobby.controller.js
@@ -52,8 +52,9 @@ window.onload = function() {
   function ud_create_cookie(UD_NAME, UD_VALUE) {
 
     var UD_DATUM = new Date();
+    console.log(UD_DATUM);
     UD_DATUM.setTime(UD_DATUM.getTime() + (UD_DATUM*24*60*60*50));
-
+    console.log(UD_DATUM);
     var UD_DELETE = "expires="+UD_DATUM.toUTCString();
 
     document.cookie = UD_NAME + '=' + UD_VALUE + ';' + UD_DELETE;
