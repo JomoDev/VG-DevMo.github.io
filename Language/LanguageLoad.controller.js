a7c786ad394fe293a8377c07b733a4afbdaddc5e
VG-DevMo <jonas-gruempel@gmx.de>
2018-09-10 13:01:34 +0200

    Update LanguageLoad.controller.js

--- Language/LanguageLoad.controller.js
+++ Language/LanguageLoad.controller.js
@@ -9,3 +9,34 @@ function getBrowserLanguage () {
   }
   return lang;
 }
+
+function loadLanguageFromServer (lang) {
+  var path;
+  
+  switch (lang) {
+    case 'de':
+      path = 'Language/de.json';
+      break;
+    case 'en':
+      path = 'Language/en.json';
+      break;
+    default:
+      path = 'Language/en.json';
+      break;
+  }
+  
+  var objString;
+  
+  $.ajax({ url: path,
+         type: 'GET',
+         success: function(output) {
+          objString = output.toString();
+         },
+        error: function() {
+          console.log("error");
+        }
+  });
+  
+  return JSON.parse(objString);
+  
+}
