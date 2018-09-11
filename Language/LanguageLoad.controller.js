24a36cd54e3bd9d10a9c8e0e648a192fb101ad6a
VG-DevMo <jonas-gruempel@gmx.de>
2018-09-11 13:40:17 +0200

    Update LanguageLoad.controller.js

--- Language/LanguageLoad.controller.js
+++ Language/LanguageLoad.controller.js
@@ -32,7 +32,8 @@ function loadLanguageFromServer (lang) {
   
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
+        var myArr = this.responseText;
+        return myArr;
     } else {
        console.error('failed to load language file ' + lang + ' from path ' + path);
     }
